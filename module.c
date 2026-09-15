61e72f095daf4f50217cf81d1d9d099fa4f07494
PengXing791 <3531365039@qq.com>
2026-09-16 02:29:51 +0800

    Update module.c

--- module.c
+++ module.c
@@ -1,31 +1,96 @@
 #include <kpm.h>
-#include <sys/fs.h>
+#include <fs.h>
+#include <linux/dcache.h>
 
 KPM_MODULE("cpuspoof");
 
 static const char fake_cpuinfo[] =
-"processor\t: 0\n"
-"model name\t: Snapdragon 8 Gen2\n"
-"cpu MHz\t\t: 3200\n"
-"Hardware\t: Qualcomm SM8475\n";
+"Processor       : AArch64 Processor rev 0 (aarch64)\n"
+"Features        : fp asimd evtstrm aes pmull sha1 sha2 crc32 atomics fphp asimdhp\n"
+"CPU implementer : 0x48\n"
+"CPU architecture: 8\n"
+"CPU variant     : 0x1\n"
+"CPU part        : 0xd0c\n"
+"CPU revision    : 0\n\n"
+"processor       : 0\n"
+"BogoMIPS        : 26.00\n"
+"CPU implementer : 0x48\n"
+"CPU architecture: 8\n"
+"CPU variant     : 0x1\n"
+"CPU part        : 0xd0c\n"
+"CPU revision    : 0\n\n"
+"processor       : 1\n"
+"BogoMIPS        : 26.00\n"
+"CPU implementer : 0x48\n"
+"CPU architecture: 8\n"
+"CPU variant     : 0x2\n"
+"CPU part        : 0xd0a\n"
+"CPU revision    : 0\n\n"
+"processor       : 2\n"
+"BogoMIPS        : 26.00\n"
+"CPU implementer : 0x48\n"
+"CPU architecture: 8\n"
+"CPU variant     : 0x2\n"
+"CPU part        : 0xd0a\n"
+"CPU revision    : 0\n\n"
+"processor       : 3\n"
+"BogoMIPS        : 26.00\n"
+"CPU implementer : 0x48\n"
+"CPU architecture: 8\n"
+"CPU variant     : 0x2\n"
+"CPU part        : 0xd0a\n"
+"CPU revision    : 0\n\n"
+"processor       : 4\n"
+"BogoMIPS        : 26.00\n"
+"CPU implementer : 0x48\n"
+"CPU architecture: 8\n"
+"CPU variant     : 0x3\n"
+"CPU part        : 0xd0b\n"
+"CPU revision    : 0\n\n"
+"processor       : 5\n"
+"BogoMIPS        : 26.00\n"
+"CPU implementer : 0x48\n"
+"CPU architecture: 8\n"
+"CPU variant     : 0x3\n"
+"CPU part        : 0xd0b\n"
+"CPU revision    : 0\n\n"
+"processor       : 6\n"
+"BogoMIPS        : 26.00\n"
+"CPU implementer : 0x48\n"
+"CPU architecture: 8\n"
+"CPU variant     : 0x3\n"
+"CPU part        : 0xd0b\n"
+"CPU revision    : 0\n\n"
+"processor       : 7\n"
+"BogoMIPS        : 26.00\n"
+"CPU implementer : 0x48\n"
+"CPU architecture: 8\n"
+"CPU variant     : 0x3\n"
+"CPU part        : 0xd0b\n"
+"CPU revision    : 0\n\n"
+"Hardware        : Kirin9000S\n";
 
-static long (*original_read)(struct file *file, char __user *buf, size_t count, loff_t *ppos);
+static ssize_t (*orig_proc_file_read)(struct file *file, char __user *buf, size_t count, loff_t *ppos);
 
-static long hooked_read(struct file *file, char __user *buf, size_t count, loff_t *ppos)
+static ssize_t hook_proc_file_read(struct file *file, char __user *buf, size_t count, loff_t *ppos)
 {
-    if(strcmp(file->f_path.dentry->d_name.name,"cpuinfo")==0){
+    const struct dentry *dent = file->f_path.dentry;
+    if (dent && !strcmp(dent->d_name.name, "cpuinfo"))
+    {
         size_t len = strlen(fake_cpuinfo);
-        if(*ppos >= len) return 0;
-        if(count > len - *ppos) count = len - *ppos;
+        if (*ppos >= len)
+            return 0;
+        if (count > len - *ppos)
+            count = len - *ppos;
         copy_to_user(buf, fake_cpuinfo + *ppos, count);
         *ppos += count;
         return count;
     }
-    return original_read(file,buf,count,ppos);
+    return orig_proc_file_read(file, buf, count, ppos);
 }
 
 int kpm_main(void)
 {
-    KPM_HOOK_SYMBOL_FS(read, original_read, hooked_read);
+    KPM_HOOK(proc_file_read, orig_proc_file_read, hook_proc_file_read);
     return 0;
 }
